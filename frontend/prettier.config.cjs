e12219bb183de944cb74a64c915936b7ff95f42b
Daniil Trusov <daniiltrusov00@gmail.com>
2025-06-19 13:54:55 +0300

    Basic frontend initialization, added favicon and placeholder image in /public folder

---
+++ frontend/prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
